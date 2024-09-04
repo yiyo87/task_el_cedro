61f4e116e83a0dff751333850db306e29228c92c
rodrigo vera <rodrigo.vera.cartagena@gmail.com>
2024-09-04 02:04:25 -0400

    logre crear un login sensillo con algo de estilo pero funcional en alguna medida ya que pude hacer que tomar los elementos dentro de los imput y luego convertirlos en string y eso pude evidenciarlo en logcat sale todo lo que haces dentro de la aplicacion

---
+++ app/src/main/java/com/example/taskelcedro/MainActivity.java
@@ -0,0 +1,46 @@
+package com.example.taskelcedro;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+    public void login (View v){
+        EditText campo1 =this. findViewById(R.id.id_usuario);
+        String usuario= campo1.getText().toString();
+        EditText campo2 =this.findViewById(R.id.contrasenia);
+        String contrasenia = campo2.getText().toString();
+
+        if(usuario.equals("u1") && contrasenia.equals("123")){
+            Intent i = new Intent(this, principal1.class);
+            startActivity(i);
+        }else{
+            Toast.makeText(this,"error acreditacion ",Toast.LENGTH_SHORT).show();
+        }
+
+        System.out.println(usuario+" "+contrasenia);
+
+
+    }
+}
