29d66d44ca3e491443af509a82a20e63c390601b
rodrigo_Vera <rodrigo.vera.cartagena@gmail.com>
2024-09-29 14:53:26 -0300

    aqui pude hacer la pagina principal solamente hasta donde muestra las empresas con la imagen y los botones que tendran ver la informacion de la empresa y hacer un pedido

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -40,4 +40,13 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
+
 }
+
+buildscript {
+    dependencies {
+        classpath ("com.android.tools.build:gradle:8.5.2")
+    }
+}
+
+
